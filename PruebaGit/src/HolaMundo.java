8da6be754482d129b5e8f60db649888723f10a8c
JonAingeruL <j.lobato@opendeusto.es>
2024-09-20 08:31:47 +0200

    He metio otro mensaje
    ola

--- PruebaGit/src/HolaMundo.java
+++ PruebaGit/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Adios mundo curel!!!");
 		System.out.println("Me mueroooo");
+		System.out.println("PRIMO PUNCH");
 	}
 
 }
